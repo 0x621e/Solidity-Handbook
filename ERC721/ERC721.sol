d726e33edb7c241195102542ea5ca233dcedf99a
0x621e <0x621e@gmail.com>
2022-07-17 15:05:54 +0800

    721 update comments

--- ERC721/ERC721.sol
+++ ERC721/ERC721.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.7.0) (token/ERC721/ERC721.sol)
+// 英文注释来自OpenZepplin，中文注释来自John Wayne
 
 pragma solidity ^0.8.0;
 
+// 此合约为发行NFT的主合约，共引用了3个库合约，3个接口合约和一个ERC165合约，其中ERC165又引用了IERC165接口合约
+// 所以ERC721一共直接间接引用了7个合约，后面会一一介绍
 import "./IERC721.sol";
 import "./IERC721Receiver.sol";
 import "./extensions/IERC721Metadata.sol";
@@ -16,31 +19,44 @@ import "../../utils/introspection/ERC165.sol";
  * the Metadata extension, but not including the Enumerable extension, which is available separately as
  * {ERC721Enumerable}.
  */
+
+// ERC721来自以太坊改善提案#721非同质化代币标准
+// 包含MetaData，不包含Enumerable
+// is 关键字用于引用父合约
 contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
+    // 使用Address库，用isContract来判断接受NFT地址是否为合约，如果是合约需要进一步验证合约是否支持onERC721Received
     using Address for address;
+    // 使用Strings库，后面调用toString()，将uint256转换为string
     using Strings for uint256;
 
     // Token name
+    // 要发行NFT的名称
     string private _name;
 
     // Token symbol
+    // 要发行NFT的简称
     string private _symbol;
 
     // Mapping from token ID to owner address
+    // NFT ID到NFT owner地址的映射
     mapping(uint256 => address) private _owners;
 
     // Mapping owner address to token count
+    // owner地址到owner NFT数量的映射
     mapping(address => uint256) private _balances;
 
     // Mapping from token ID to approved address
+    // NFT ID到operator地址的映射
     mapping(uint256 => address) private _tokenApprovals;
 
     // Mapping from owner to operator approvals
+    // owner地址到operator地址的批量授权映射
     mapping(address => mapping(address => bool)) private _operatorApprovals;
 
     /**
      * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
      */
+    // 构造函数，初始化NFT名称和简称
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
@@ -49,7 +65,14 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev See {IERC165-supportsInterface}.
      */
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
+    // 实现IERC165接口supportsInterface，智能合约可以声明它支持的接口，供其他合约检查
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC165, IERC165)
+        returns (bool)
+    {
         return
             interfaceId == type(IERC721).interfaceId ||
             interfaceId == type(IERC721Metadata).interfaceId ||
@@ -59,15 +82,32 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev See {IERC721-balanceOf}.
      */
-    function balanceOf(address owner) public view virtual override returns (uint256) {
-        require(owner != address(0), "ERC721: address zero is not a valid owner");
+    // 实现IERC721的balanceOf，查询owner地址的NFT数量，owner地址不可以是0地址
+    function balanceOf(address owner)
+        public
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        require(
+            owner != address(0),
+            "ERC721: address zero is not a valid owner"
+        );
         return _balances[owner];
     }
 
     /**
      * @dev See {IERC721-ownerOf}.
      */
-    function ownerOf(uint256 tokenId) public view virtual override returns (address) {
+    // 利用NFT ID查询owner地址，尚未mint的NFT不可查询
+    function ownerOf(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (address)
+    {
         address owner = _owners[tokenId];
         require(owner != address(0), "ERC721: invalid token ID");
         return owner;
@@ -76,6 +116,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev See {IERC721Metadata-name}.
      */
+    // 查询NFT名称
     function name() public view virtual override returns (string memory) {
         return _name;
     }
@@ -83,6 +124,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev See {IERC721Metadata-symbol}.
      */
+    // 查询NFT简称
     function symbol() public view virtual override returns (string memory) {
         return _symbol;
     }
@@ -90,11 +132,21 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev See {IERC721Metadata-tokenURI}.
      */
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    // 查询tokenURI，metadata存储地址
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
         _requireMinted(tokenId);
 
         string memory baseURI = _baseURI();
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, tokenId.toString()))
+                : "";
     }
 
     /**
@@ -102,6 +154,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
      * by default, can be overridden in child contracts.
      */
+    // tokenURI就是把baseURI和tokenId拼接在一起，需要在子合约override，比如ipfs://abcdefg/
     function _baseURI() internal view virtual returns (string memory) {
         return "";
     }
@@ -109,6 +162,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev See {IERC721-approve}.
      */
+    // 将tokenId授权给to地址，to不可以是owner，且msg.sender是owner或授权地址
     function approve(address to, uint256 tokenId) public virtual override {
         address owner = ERC721.ownerOf(tokenId);
         require(to != owner, "ERC721: approval to current owner");
@@ -124,7 +178,14 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev See {IERC721-getApproved}.
      */
-    function getApproved(uint256 tokenId) public view virtual override returns (address) {
+    // 查询tokenId的授权operator地址，tokenId需要已经被minted
+    function getApproved(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (address)
+    {
         _requireMinted(tokenId);
 
         return _tokenApprovals[tokenId];
@@ -133,27 +194,43 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev See {IERC721-setApprovalForAll}.
      */
-    function setApprovalForAll(address operator, bool approved) public virtual override {
+    // 将owner地址所有该系列NFT授权给operator，很多钓鱼网站就是调用此函数盗取NFT
+    function setApprovalForAll(address operator, bool approved)
+        public
+        virtual
+        override
+    {
         _setApprovalForAll(_msgSender(), operator, approved);
     }
 
     /**
      * @dev See {IERC721-isApprovedForAll}.
      */
-    function isApprovedForAll(address owner, address operator) public view virtual override returns (bool) {
+    // 查询owner是否已经授权该operator地址
+    function isApprovedForAll(address owner, address operator)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
         return _operatorApprovals[owner][operator];
     }
 
     /**
      * @dev See {IERC721-transferFrom}.
      */
+    // 非安全转账，不推荐
     function transferFrom(
         address from,
         address to,
         uint256 tokenId
     ) public virtual override {
         //solhint-disable-next-line max-line-length
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner nor approved");
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not token owner nor approved"
+        );
 
         _transfer(from, to, tokenId);
     }
@@ -161,6 +238,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev See {IERC721-safeTransferFrom}.
      */
+    // 安全转账重载函数
     function safeTransferFrom(
         address from,
         address to,
@@ -172,13 +250,17 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev See {IERC721-safeTransferFrom}.
      */
+    // 安全转账，调用_safeTransfer
     function safeTransferFrom(
         address from,
         address to,
         uint256 tokenId,
         bytes memory data
     ) public virtual override {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner nor approved");
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not token owner nor approved"
+        );
         _safeTransfer(from, to, tokenId, data);
     }
 
@@ -200,6 +282,9 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      *
      * Emits a {Transfer} event.
      */
+    // 安全转账，安全地将tokenId从from转移到to，会检查合约接收者是否了解ERC721协议，以防止代币被永久锁定
+    // 调用了_transfer函数和_checkOnERC721Received函数，from不能是0地址，to不能是0地址，tokenId必须存在，并且被from拥有
+    // 如果to是智能合约, 他必须支持IERC721Receiver-onERC721Received
     function _safeTransfer(
         address from,
         address to,
@@ -207,7 +292,10 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
         bytes memory data
     ) internal virtual {
         _transfer(from, to, tokenId);
-        require(_checkOnERC721Received(from, to, tokenId, data), "ERC721: transfer to non ERC721Receiver implementer");
+        require(
+            _checkOnERC721Received(from, to, tokenId, data),
+            "ERC721: transfer to non ERC721Receiver implementer"
+        );
     }
 
     /**
@@ -218,6 +306,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      * Tokens start existing when they are minted (`_mint`),
      * and stop existing when they are burned (`_burn`).
      */
+    // 查询tokenId是否存在，被minted，即owner是否为0
     function _exists(uint256 tokenId) internal view virtual returns (bool) {
         return _owners[tokenId] != address(0);
     }
@@ -229,9 +318,17 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      *
      * - `tokenId` must exist.
      */
-    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual returns (bool) {
+    // 查询spender地址是否已经被授权某个tokenId
+    function _isApprovedOrOwner(address spender, uint256 tokenId)
+        internal
+        view
+        virtual
+        returns (bool)
+    {
         address owner = ERC721.ownerOf(tokenId);
-        return (spender == owner || isApprovedForAll(owner, spender) || getApproved(tokenId) == spender);
+        return (spender == owner ||
+            isApprovedForAll(owner, spender) ||
+            getApproved(tokenId) == spender);
     }
 
     /**
@@ -244,6 +341,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      *
      * Emits a {Transfer} event.
      */
+    // 安全mint重载函数
     function _safeMint(address to, uint256 tokenId) internal virtual {
         _safeMint(to, tokenId, "");
     }
@@ -252,6 +350,8 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      * @dev Same as {xref-ERC721-_safeMint-address-uint256-}[`_safeMint`], with an additional `data` parameter which is
      * forwarded in {IERC721Receiver-onERC721Received} to contract recipients.
      */
+    // 安全mint，tokenId必须并没有被mint，查询to地址是否支持IERC721Receiver-onERC721Received
+    // 本质是从0地址transfer到to地址
     function _safeMint(
         address to,
         uint256 tokenId,
@@ -276,6 +376,9 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      *
      * Emits a {Transfer} event.
      */
+    // 铸造函数，通过调整_balances和_owners变量来铸造tokenId并转账给to，同时释放Tranfer事件
+    // 这个mint函数所有人都能调用，实际使用需要开发人员重写，加上一些条件
+    // 要求tokenId尚不存在，to不是0地址
     function _mint(address to, uint256 tokenId) internal virtual {
         require(to != address(0), "ERC721: mint to the zero address");
         require(!_exists(tokenId), "ERC721: token already minted");
@@ -300,6 +403,8 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      *
      * Emits a {Transfer} event.
      */
+    // 销毁函数，通过调整_balances和_owners变量来销毁tokenId，同时释放Tranfer事件
+    // 要求tokenId存在
     function _burn(uint256 tokenId) internal virtual {
         address owner = ERC721.ownerOf(tokenId);
 
@@ -327,12 +432,17 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      *
      * Emits a {Transfer} event.
      */
+    // 转账函数，通过调整_balances和_owner变量将tokenId从from转账给to，同时释放Tranfer事件
+    // 要求tokenId被from拥有，to不是0地址
     function _transfer(
         address from,
         address to,
         uint256 tokenId
     ) internal virtual {
-        require(ERC721.ownerOf(tokenId) == from, "ERC721: transfer from incorrect owner");
+        require(
+            ERC721.ownerOf(tokenId) == from,
+            "ERC721: transfer from incorrect owner"
+        );
         require(to != address(0), "ERC721: transfer to the zero address");
 
         _beforeTokenTransfer(from, to, tokenId);
@@ -354,6 +464,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      *
      * Emits an {Approval} event.
      */
+    // 授权tokenId到to地址
     function _approve(address to, uint256 tokenId) internal virtual {
         _tokenApprovals[tokenId] = to;
         emit Approval(ERC721.ownerOf(tokenId), to, tokenId);
@@ -364,6 +475,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      *
      * Emits an {ApprovalForAll} event.
      */
+    // 授权owner地址所有该系列的NFT到operator地址
     function _setApprovalForAll(
         address owner,
         address operator,
@@ -377,6 +489,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev Reverts if the `tokenId` has not been minted yet.
      */
+    // 查询某tokenId是否被minted
     function _requireMinted(uint256 tokenId) internal view virtual {
         require(_exists(tokenId), "ERC721: invalid token ID");
     }
@@ -391,6 +504,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      * @param data bytes optional data to send along with the call
      * @return bool whether the call correctly returned the expected magic value
      */
+    // 用于在to为合约的时候调用IERC721Receiver-onERC721Received, 以防tokenId被不小心转入黑洞
     function _checkOnERC721Received(
         address from,
         address to,
@@ -398,11 +512,20 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
         bytes memory data
     ) private returns (bool) {
         if (to.isContract()) {
-            try IERC721Receiver(to).onERC721Received(_msgSender(), from, tokenId, data) returns (bytes4 retval) {
+            try
+                IERC721Receiver(to).onERC721Received(
+                    _msgSender(),
+                    from,
+                    tokenId,
+                    data
+                )
+            returns (bytes4 retval) {
                 return retval == IERC721Receiver.onERC721Received.selector;
             } catch (bytes memory reason) {
                 if (reason.length == 0) {
-                    revert("ERC721: transfer to non ERC721Receiver implementer");
+                    revert(
+                        "ERC721: transfer to non ERC721Receiver implementer"
+                    );
                 } else {
                     /// @solidity memory-safe-assembly
                     assembly {
