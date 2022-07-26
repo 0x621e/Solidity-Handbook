85624e261c4e26e22293418b2b404de487383673
0x621e <0x621e@gmail.com>
2022-07-26 18:14:32 +0800

    update

--- Examples/SimpleStorage/FundMe/PriceConverter.sol
+++ Examples/SimpleStorage/FundMe/PriceConverter.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-// 用户可以发送eth到FundMe，并设定发送eth最小额度，以美元计价
-// 有提取功能withdraw
+// 需要chainlink去中心化预言机提供eth实时价格，将相关功能打包进此库
 
 pragma solidity 0.8.8;
 
@@ -8,16 +7,19 @@ pragma solidity 0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
+    // 得到eth当前价格
     function getPrice() internal view returns (uint256) {
         // ABI
         // Rinkeby测试网上的 ETH/USD价格合约地址: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
             0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
         );
+        //只需要其中的price数据
         (, int price, , , ) = priceFeed.latestRoundData();
         return uint256(price * 1e10);
     }
 
+    // 得到相应eth对应美元的价值
     function getConvertionRate(uint256 ethAmount)
         internal
         view
@@ -28,6 +30,7 @@ library PriceConverter {
         return ethAmountInUsd;
     }
 
+    // 版本
     function getVersion() internal view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
             0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
